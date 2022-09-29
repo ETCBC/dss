9e073d22e7a0bce9f93682e67e4a87dd98d65fc2
Martijn N <martijn.naayer@chello.nl>
2022-09-29 12:39:41 +0200

    corrected several values in lex_etcbc and sp_etcbc, added tests

--- tests/test_data_dss.py
+++ tests/test_data_dss.py
@@ -2,7 +2,7 @@ import logging
 import pytest
 
 from tf.app import use
-DSS = use('etcbc/dss:clone', checkout='clone', version='1.5', provenanceSpec=dict(moduleSpecs=[]))
+DSS = use('etcbc/dss:clone', checkout='clone', version='1.6', provenanceSpec=dict(moduleSpecs=[]))
 Fdss, Ldss, Tdss = DSS.api.F, DSS.api.L, DSS.api.T
 
 
@@ -13,13 +13,15 @@ logging.basicConfig(
     format='%(name)s - %(levelname)s - %(message)s')
 
 
-def test_lexemes_nouns_ending():
+def test_lexemes_adjv_subs_verb_endings():
     try:
-        assert all([Fdss.lex_etcbc.v(w)[-1] == '/' for w in Fdss.otype.s('word') if Fdss.lex_etcbc.v(w) and Fdss.sp_etcbc.v(w) == 'subs'])
-        logging.info("Testing lexemes_nouns_ending: SUCCES")
+        #assert all(Fdss.lex_etcbc.v(w)[-1] == '/' for w in Fdss.otype.s('word') if Fdss.lex_etcbc.v(w) and Fdss.sp_etcbc.v(w) in {'adjv', 'subs'})
+        assert all(Fdss.lex_etcbc.v(w)[-1] == '[' for w in Fdss.otype.s('word') if Fdss.lex_etcbc.v(w) and Fdss.sp_etcbc.v(w) == 'verb')
+        logging.info("Testing adjv_subs_verb_endings: SUCCES")
     except AssertionError as err:
-        logging.error("Testing lexemes_nouns_ending: there is at least one word without '/' at the end")
+        logging.error("Testing lexemes_nouns_ending: there is at least one word without '/' or '[' at the end")
         raise err
+
 
 if __name__ == "__main__":
     test_lexemes_nouns_ending()
