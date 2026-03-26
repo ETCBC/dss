7900a2af8cef4329b871d6b56f6039fc79ce8502
ThijsAmersfoort <tamersfoort@gmail.com>
2026-03-26 08:44:40 +0100

    TempleScroll imported into TF 1.9.1
    Some corrections made to dss_out data, 10 features extracted from morphological code and exported to TF version 1.9.1

---
+++ tf/1.9.1/note_etcbc.tf
@@ -0,0 +1,50 @@
+@node
+@acronym=dss-additions
+@convertedBy=Martijn Naaijer, Dirk Roorda and Thijs Amersfoort
+@createdBy=Martin G. Abegg, Martijn Naaijer, ETCBC
+@createdDate=2026
+@description=Notes by Thijs Amersfoort during the morphological parsing of the text.
+@license=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenseUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@source=Automatic generated morphological parsing corrected by Thijs Amersfoort
+@valueType=str
+@writtenBy=Text-Fabric
+@dateWritten=2026-03-26T07:35:00+00:00Z
+
+1867215	Is de suffix zo goed?
+1867262	Abegg parses as qal, Deut 7:26 has a piel
+1867340	Laatste deel is afgebroken, incompleet. Hoe geven we dat aan in de codering?
+1867381	Overschreven yod, nog een ander teken?
+1867483	Ook hier weer afgebroken stuk
+1867506	Weer afgebroken
+1867524	Weer afgebroken
+1867527	Overschreven woord?
+1867608	Afgebroken
+1867626	Afgebroken
+1867665	Afgebroken
+1867706	Niet helemaal zeker van de vowel patterns hier.
+1867727	Afgebroken
+1867733	Afgebroken
+1867817	Afgebroken vermoed ik
+1868317	Abegg maakt er een lang perfectum 2m plur van, de Duitse vertaling (du sollst sie ...) veronderstelt 2m sing met 3m plur suffix. In Qimrons tabellen van Hebreeuws in de Dode Zeerollen is de lange vorm van Abegg terug te vinden, een passend 3m plur suffix op +MH niet. Heb nu &H voor de lange vorm.
+1868505	Lex CWRH/ is een muur. Zou ook naar olijven/druivenplanten kunnen verwijzen, dan CWRH=/ aanmaken. Maar context is te onduidelijk.
+1868785	Klopt dit wel? Woord tweemaal herhaald hier, terwijl boek regel begint met ולכבשׂ
+1870326	Lex lijkt hier verkeerd in de database, voorste alef mist
+1870694	Volgorde dit en volgende woord is omgedraaid in Duitse uitgave
+1871171	Hier hebben we een omwisseling van B en R, zo goed weergegeven?
+1871541	Omwisseling zayin en alef
+1871650	Zo ook in Lev 5:5. Waarom hitpa''el niet :d?
+1872364	Dit moet Betel zijn, BJT_>L, hoe wil je deze twee samen hebben?
+1872391	Abegg heeft bij dit woord een ?. Moeten we onzekerheid er nog in verwerken?
+Van dit woord is het suffix afgebroken. Hoe moet dat erin staan?
+1872595	Hier is de waw toegevoegd maar aangeduid als onbekend
+1873543	Ik weet niet zeker of dit het goede lexeem, in het Duits: säulenvorbaus
+1873667	Hier de consonantentext verbeterd. Er was een extra waw van een eerder gedeelte dat afgebroken is meegenomen.
+1873739	In de tekst zit er een spatie tussen eerste deel, WLY en rest van het woord.
+1876028	Hier was een C gezet, maar moet een F zijn
+1877554	Methatesis? > op verkeerde plek
+1877703	Of ~H?
+1877729	Zo ook in Exo 3:21, maar is dit geen ~N?
+1879569	Hier de bhsa gevolgd in :d achter dit werkwoord, maar is een poel, eigenlijk geen verdubbeling.
+1880663	Welke modifiers krijgt een poal mee?
+1881082	Of R(>(H&W&J[/H:p
