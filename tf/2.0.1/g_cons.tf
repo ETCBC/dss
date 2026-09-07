7a32ac5575d8eacb7269869b14e9968b7e416791
ThijsAmersfoort <tamersfoort@gmail.com>
2026-09-07 15:02:39 +0200

    First version of 1Qisaa, aligment code in separate folder

---
+++ tf/2.0.1/g_cons.tf
@@ -0,0 +1,13 @@
+@node
+@acronym=dss-additions
+@convertedBy=Martijn Naaijer, Dirk Roorda and Thijs Amersfoort
+@createdBy=Martin G. Abegg, Martijn Naaijer, ETCBC
+@createdDate=2026
+@description=The consonantal representation of a word or sign in BHSA transliteration.
+@license=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenseUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@source=Automatic generated morphological parsing corrected by Thijs Amersfoort
+@valueType=str
+@writtenBy=Text-Fabric
+@dateWritten=2026-09-07T12:45:26+00:00Z
+
---
+++ tf/2.0.1/note_etcbc.tf
@@ -0,0 +1,18 @@
+@node
+@acronym=dss-additions
+@convertedBy=Martijn Naaijer, Dirk Roorda and Thijs Amersfoort
+@createdBy=Martin G. Abegg, Martijn Naaijer, ETCBC
+@createdDate=2026
+@description=Notes by Thijs Amersfoort during the morphological parsing of the text.
+@license=Creative Commons Attribution-NonCommercial 4.0 International License
+@licenseUrl=http://creativecommons.org/licenses/by-nc/4.0/
+@source=Automatic generated morphological parsing corrected by Thijs Amersfoort
+@valueType=str
+@writtenBy=Text-Fabric
+@dateWritten=2026-09-07T12:45:27+00:00Z
+
+1897703	BHSA heeft parsing: W-MFWF/:c
+1903175	Mogelijk schrijffout van de schrijver, MT heeft <YWT. Nu als onbekend lexeem opgevat.
+1910972	Hier een Aramees suffix (Qimron), zodat de dualis niet door /J= maar door /W= wordt weergegeven.
+1914675	Afgebroken, maar waarschijnlijk een suffix, vandaar /=
+1916700	Nu uitgegaag van verschrijving, wellicht afgeleid van N<Y, "prikken", maar is bekend werkwoord in BH
